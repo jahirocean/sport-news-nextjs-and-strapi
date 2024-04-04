e6e8a54284ff72ba83021ebc648f4502154c4dbb
Zahir Raihan <zahirraihann@gmail.com>
2024-04-04 22:19:20 +0600

    First commit

---
+++ sport-news-backend-strapi/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9070ce55d9d93ac0825c68905697ee0'),
+    },
+  },
+});
---
+++ sport-news-backend-strapi/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1695f662-1305-4ff3-92e3-6458c65289a0'
+};
